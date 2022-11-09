adf86eff1f9629da0d76599c33bc3e460be2589f
Dmitry Ilyin <dima@doty.ru>
2022-11-09 12:41:16 +0300

    Fill all global clocks table

--- libraries/sensor/hi3516ev200/sony_imx307_2L/imx307_2l_sensor_ctl.c
+++ libraries/sensor/hi3516ev200/sony_imx307_2L/imx307_2l_sensor_ctl.c
@@ -304,34 +304,157 @@ void imx307_2l_init_universal(VI_PIPE ViPipe, const char *name,
 	imx307_2l_write_register(ViPipe, 0x31EC, 0x37); // ADBIT3
 
 	imx307_2l_write_register(ViPipe, 0x3405, 0x10); // REPETITION
-	imx307_2l_write_register(ViPipe, 0x3407, 0x01); // PHYSICAL_LANE_NUM
 	imx307_2l_write_register(ViPipe, 0x3414, 0x0A); // OPB_SIZE_V
 	imx307_2l_write_register(ViPipe, 0x3418, 0x49); // Y_OUT_SIZE
 	imx307_2l_write_register(ViPipe, 0x3419, 0x04); // Y_OUT_SIZE
 	imx307_2l_write_register(ViPipe, 0x3441, 0x0A); // CSI_DT_FMT
 	imx307_2l_write_register(ViPipe, 0x3442, 0x0A); // CSI_DT_FMT
-	imx307_2l_write_register(ViPipe, 0x3443, 0x01); // CSI_LANE_MODE
+
+	if (mipi_lanes == MIPI_LANES_2) {
+		imx307_2l_write_register(ViPipe, 0x3443, 0x01); // CSI_LANE_MODE
+		imx307_2l_write_register(ViPipe, 0x3407,
+					 0x01); // PHYSICAL_LANE_NUM
+	} else if (mipi_lanes == MIPI_LANES_4) {
+		imx307_2l_write_register(ViPipe, 0x3443, 0x03); // CSI_LANE_MODE
+		imx307_2l_write_register(ViPipe, 0x3407,
+					 0x03); // PHYSICAL_LANE_NUM
+	}
+
 	imx307_2l_write_register(ViPipe, 0x3444, 0x20); // EXTCK_FREQ
 	imx307_2l_write_register(ViPipe, 0x3445, 0x25); // EXTCK_FREQ
 
 	if (winmode == WINMODE_720P) {
-		imx307_2l_write_register(ViPipe, 0x3446, 0x67); // TCLKPOST
-		imx307_2l_write_register(ViPipe, 0x3448, 0x57); // THSZERO
-		imx307_2l_write_register(ViPipe, 0x344A, 0x2F); // THSPREPARE
-		imx307_2l_write_register(ViPipe, 0x344C, 0x27); // TCLKTRAIL
-		imx307_2l_write_register(ViPipe, 0x344E, 0x2F); // THSTRAIL
-		imx307_2l_write_register(ViPipe, 0x3450, 0xBF); // TCLKZERO
-		imx307_2l_write_register(ViPipe, 0x3452, 0x2F); // TCLKPREPARE
-		imx307_2l_write_register(ViPipe, 0x3454, 0x27); // TLPX
+		if (mipi_lanes == MIPI_LANES_2 && fps <= 30) {
+			imx307_2l_write_register(ViPipe, 0x3446,
+						 0x4F); // TCLKPOST
+			imx307_2l_write_register(ViPipe, 0x3448,
+						 0x2F); // THSZERO
+			imx307_2l_write_register(ViPipe, 0x344A,
+						 0x17); // THSPREPARE
+			imx307_2l_write_register(ViPipe, 0x344C,
+						 0x17); // TCLKTRAIL
+			imx307_2l_write_register(ViPipe, 0x344E,
+						 0x17); // THSTRAIL
+			imx307_2l_write_register(ViPipe, 0x3450,
+						 0x57); // TCLKZERO
+			imx307_2l_write_register(ViPipe, 0x3452,
+						 0x17); // TCLKPREPARE
+			imx307_2l_write_register(ViPipe, 0x3454, 0x17); // TLPX
+		} else if (mipi_lanes == MIPI_LANES_2 && fps > 30) {
+			imx307_2l_write_register(ViPipe, 0x3446,
+						 0x67); // TCLKPOST
+			imx307_2l_write_register(ViPipe, 0x3448,
+						 0x57); // THSZERO
+			imx307_2l_write_register(ViPipe, 0x344A,
+						 0x2F); // THSPREPARE
+			imx307_2l_write_register(ViPipe, 0x344C,
+						 0x27); // TCLKTRAIL
+			imx307_2l_write_register(ViPipe, 0x344E,
+						 0x2F); // THSTRAIL
+			imx307_2l_write_register(ViPipe, 0x3450,
+						 0xBF); // TCLKZERO
+			imx307_2l_write_register(ViPipe, 0x3452,
+						 0x2F); // TCLKPREPARE
+			imx307_2l_write_register(ViPipe, 0x3454, 0x27); // TLPX
+		} else if (mipi_lanes == MIPI_LANES_4 && fps <= 30) {
+			imx307_2l_write_register(ViPipe, 0x3446,
+						 0x47); // TCLKPOST
+			imx307_2l_write_register(ViPipe, 0x3448,
+						 0x17); // THSZERO
+			imx307_2l_write_register(ViPipe, 0x344A,
+						 0x0F); // THSPREPARE
+			imx307_2l_write_register(ViPipe, 0x344C,
+						 0x0F); // TCLKTRAIL
+			imx307_2l_write_register(ViPipe, 0x344E,
+						 0x0F); // THSTRAIL
+			imx307_2l_write_register(ViPipe, 0x3450,
+						 0x2B); // TCLKZERO
+			imx307_2l_write_register(ViPipe, 0x3452,
+						 0x0B); // TCLKPREPARE
+			imx307_2l_write_register(ViPipe, 0x3454, 0x0F); // TLPX
+		} else if (mipi_lanes == MIPI_LANES_4 && fps > 30) {
+			imx307_2l_write_register(ViPipe, 0x3446,
+						 0x4F); // TCLKPOST
+			imx307_2l_write_register(ViPipe, 0x3448,
+						 0x2F); // THSZERO
+			imx307_2l_write_register(ViPipe, 0x344A,
+						 0x17); // THSPREPARE
+			imx307_2l_write_register(ViPipe, 0x344C,
+						 0x17); // TCLKTRAIL
+			imx307_2l_write_register(ViPipe, 0x344E,
+						 0x17); // THSTRAIL
+			imx307_2l_write_register(ViPipe, 0x3450,
+						 0x57); // TCLKZERO
+			imx307_2l_write_register(ViPipe, 0x3452,
+						 0x17); // TCLKPREPARE
+			imx307_2l_write_register(ViPipe, 0x3454, 0x17); // TLPX
+		}
 	} else if (winmode == WINMODE_1080P) {
-		imx307_2l_write_register(ViPipe, 0x3446, 0x57); // TCLKPOST
-		imx307_2l_write_register(ViPipe, 0x3448, 0x37); // THSZERO
-		imx307_2l_write_register(ViPipe, 0x344A, 0x1F); // THSPREPARE
-		imx307_2l_write_register(ViPipe, 0x344C, 0x1F); // TCLKTRAIL
-		imx307_2l_write_register(ViPipe, 0x344E, 0x1F); // THSTRAIL
-		imx307_2l_write_register(ViPipe, 0x3450, 0x77); // TCLKZERO
-		imx307_2l_write_register(ViPipe, 0x3452, 0x1F); // TCLKPREPARE
-		imx307_2l_write_register(ViPipe, 0x3454, 0x17); // TLPX
+		if (mipi_lanes == MIPI_LANES_2 && fps <= 30) {
+			imx307_2l_write_register(ViPipe, 0x3446,
+						 0x57); // TCLKPOST
+			imx307_2l_write_register(ViPipe, 0x3448,
+						 0x37); // THSZERO
+			imx307_2l_write_register(ViPipe, 0x344A,
+						 0x1F); // THSPREPARE
+			imx307_2l_write_register(ViPipe, 0x344C,
+						 0x1F); // TCLKTRAIL
+			imx307_2l_write_register(ViPipe, 0x344E,
+						 0x1F); // THSTRAIL
+			imx307_2l_write_register(ViPipe, 0x3450,
+						 0x77); // TCLKZERO
+			imx307_2l_write_register(ViPipe, 0x3452,
+						 0x1F); // TCLKPREPARE
+			imx307_2l_write_register(ViPipe, 0x3454, 0x17); // TLPX
+		} else if (mipi_lanes == MIPI_LANES_2 && fps > 30) {
+			imx307_2l_write_register(ViPipe, 0x3446,
+						 0x77); // TCLKPOST
+			imx307_2l_write_register(ViPipe, 0x3448,
+						 0x67); // THSZERO
+			imx307_2l_write_register(ViPipe, 0x344A,
+						 0x47); // THSPREPARE
+			imx307_2l_write_register(ViPipe, 0x344C,
+						 0x37); // TCLKTRAIL
+			imx307_2l_write_register(ViPipe, 0x344E,
+						 0x3F); // THSTRAIL
+			imx307_2l_write_register(ViPipe, 0x3450,
+						 0xFF); // TCLKZERO
+			imx307_2l_write_register(ViPipe, 0x3452,
+						 0x3F); // TCLKPREPARE
+			imx307_2l_write_register(ViPipe, 0x3454, 0x37); // TLPX
+		} else if (mipi_lanes == MIPI_LANES_4 && fps <= 30) {
+			imx307_2l_write_register(ViPipe, 0x3446,
+						 0x47); // TCLKPOST
+			imx307_2l_write_register(ViPipe, 0x3448,
+						 0x1F); // THSZERO
+			imx307_2l_write_register(ViPipe, 0x344A,
+						 0x17); // THSPREPARE
+			imx307_2l_write_register(ViPipe, 0x344C,
+						 0x0F); // TCLKTRAIL
+			imx307_2l_write_register(ViPipe, 0x344E,
+						 0x17); // THSTRAIL
+			imx307_2l_write_register(ViPipe, 0x3450,
+						 0x47); // TCLKZERO
+			imx307_2l_write_register(ViPipe, 0x3452,
+						 0x0F); // TCLKPREPARE
+			imx307_2l_write_register(ViPipe, 0x3454, 0x0F); // TLPX
+		} else if (mipi_lanes == MIPI_LANES_4 && fps > 30) {
+			imx307_2l_write_register(ViPipe, 0x3446,
+						 0x57); // TCLKPOST
+			imx307_2l_write_register(ViPipe, 0x3448,
+						 0x37); // THSZERO
+			imx307_2l_write_register(ViPipe, 0x344A,
+						 0x1F); // THSPREPARE
+			imx307_2l_write_register(ViPipe, 0x344C,
+						 0x1F); // TCLKTRAIL
+			imx307_2l_write_register(ViPipe, 0x344E,
+						 0x1F); // THSTRAIL
+			imx307_2l_write_register(ViPipe, 0x3450,
+						 0x77); // TCLKZERO
+			imx307_2l_write_register(ViPipe, 0x3452,
+						 0x1F); // TCLKPREPARE
+			imx307_2l_write_register(ViPipe, 0x3454, 0x17); // TLPX
+		}
 	}
 	// Clear clock high halves
 	imx307_2l_write_register(ViPipe, 0x3447, 0x00);
@@ -380,13 +503,16 @@ void imx307_2l_wdr_1080p30_2to1_init(VI_PIPE ViPipe)
 	imx307_2l_write_register(ViPipe, 0x3000, 0x01); //  standby
 
 	imx307_2l_write_register(ViPipe, 0x3002, 0x00); //  XMSTA;
-	imx307_2l_write_register(ViPipe, 0x3005, 0x00); //  ADBIT 0x00->10Bit;
+	imx307_2l_write_register(ViPipe, 0x3005,
+				 0x00); //  ADBIT 0x00->10Bit;
 	imx307_2l_write_register(ViPipe, 0x3007,
 				 0x00); // VREVERSE & HREVERSE & WINMODE
-	imx307_2l_write_register(ViPipe, 0x3009, 0x01); // FRSEL & FDG SEL
+	imx307_2l_write_register(ViPipe, 0x3009,
+				 0x01); // FRSEL & FDG SEL
 	imx307_2l_write_register(ViPipe, 0x300A, 0x3C); // BLKLEVEL
 	imx307_2l_write_register(ViPipe, 0x300C, 0x11);
-	imx307_2l_write_register(ViPipe, 0x3011, 0x0A); //  Change after reset;
+	imx307_2l_write_register(ViPipe, 0x3011,
+				 0x0A); //  Change after reset;
 	imx307_2l_write_register(ViPipe, 0x3018, 0x65); //  VMAX[7:0]
 	imx307_2l_write_register(ViPipe, 0x3019, 0x04); //  VMAX[15:8]
 	imx307_2l_write_register(ViPipe, 0x301C,
@@ -401,7 +527,8 @@ void imx307_2l_wdr_1080p30_2to1_init(VI_PIPE ViPipe)
 	imx307_2l_write_register(ViPipe, 0x3031, 0x00); //  RHS1
 	imx307_2l_write_register(ViPipe, 0x3045,
 				 0x05); //  DOLSCDEN & DOLSYDINFOEN & HINFOEN
-	imx307_2l_write_register(ViPipe, 0x3046, 0x00); //  OPORTSEL & ODBIT
+	imx307_2l_write_register(ViPipe, 0x3046,
+				 0x00); //  OPORTSEL & ODBIT
 	imx307_2l_write_register(ViPipe, 0x304B,
 				 0x0A); //  XVSOUTSEL & XHSOUTSEL
 	imx307_2l_write_register(
@@ -438,14 +565,17 @@ void imx307_2l_wdr_1080p30_2to1_init(VI_PIPE ViPipe)
 
 	// MIPI setting
 	imx307_2l_write_register(ViPipe, 0x3405, 0x00); //  REPETITION
-	imx307_2l_write_register(ViPipe, 0x3407, 0x01); //  PHYSICAL_LANE_NUM
+	imx307_2l_write_register(ViPipe, 0x3407,
+				 0x01); //  PHYSICAL_LANE_NUM
 	imx307_2l_write_register(ViPipe, 0x3414, 0x0A); //  OPB_SIZE_V
 	imx307_2l_write_register(ViPipe, 0x3415, 0x00);
 	imx307_2l_write_register(ViPipe, 0x3418, 0x9C); //  Y_OUT_SIZE
 	imx307_2l_write_register(ViPipe, 0x3419, 0x08); //  Y_OUT_SIZE
-	imx307_2l_write_register(ViPipe, 0x3441, 0x0A); //  CSI_DT_FMT 10Bit
+	imx307_2l_write_register(ViPipe, 0x3441,
+				 0x0A); //  CSI_DT_FMT 10Bit
 	imx307_2l_write_register(ViPipe, 0x3442, 0x0A);
-	imx307_2l_write_register(ViPipe, 0x3443, 0x01); //  CSI_LANE_MODE
+	imx307_2l_write_register(ViPipe, 0x3443,
+				 0x01); //  CSI_LANE_MODE
 	imx307_2l_write_register(ViPipe, 0x3444, 0x20); //  EXTCK_FREQ
 	imx307_2l_write_register(ViPipe, 0x3445, 0x25); //  EXTCK_FREQ
 	imx307_2l_write_register(ViPipe, 0x3446, 0x77); //  TCLKPOST
@@ -473,7 +603,8 @@ void imx307_2l_wdr_1080p30_2to1_init(VI_PIPE ViPipe)
 
 	imx307_2l_default_reg_init(ViPipe);
 
-	imx307_2l_write_register(ViPipe, 0x3000, 0x00); //  Standby Cancel
+	imx307_2l_write_register(ViPipe, 0x3000,
+				 0x00); //  Standby Cancel
 
 	printf("============================================================================\n");
 	printf("===Imx307_2l sensor 1080P30fps 10bit 2to1 WDR(60fps->30fps) init success!===\n");
